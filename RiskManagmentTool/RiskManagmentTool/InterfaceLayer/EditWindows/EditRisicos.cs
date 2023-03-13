ab9b8aaf35628d8a2521c36d9b6317ee4cc1174b
Maurits <mauricebrouwers373@hotmail.com>
2023-03-13 13:50:04 +0100

    cleanup unused usages decalrations

--- RiskManagmentTool/RiskManagmentTool/InterfaceLayer/EditWindows/EditRisicos.cs
+++ RiskManagmentTool/RiskManagmentTool/InterfaceLayer/EditWindows/EditRisicos.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using RiskManagmentTool.LogicLayer;
-using RiskManagmentTool.InterfaceLayer.AddWindows;
 using RiskManagmentTool.InterfaceLayer.WeergeefWindows;
 
 namespace RiskManagmentTool.InterfaceLayer.EditWindows
